903dee394fd08a5a80ded6fd0158f8b6f7c4cd35
wf-farez <fabian.farez@ucuenca.edu.ec>
2024-03-21 11:37:53 -0500

    primer commit

---
+++ src/main/java/springfluttercrud/SpringFlutterCrudApplication.java
@@ -0,0 +1,13 @@
+package springfluttercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlutterCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlutterCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springfluttercrud/SpringFlutterCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package springfluttercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlutterCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
